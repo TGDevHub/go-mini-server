4a21b277ab1e1a925a213a431712e9c374fdd748
Evgeniy Telegin <telegin.e@roistat.com>
2025-07-28 17:34:55 +0300

    edit read me

--- core/db/pool/pool_handler.go
+++ core/db/pool/pool_handler.go
@@ -3,10 +3,3 @@ package pool
 type poolConnectionHandler struct {
 	instance *pool
 }
-
-func (pc *poolConnectionHandler) Close() {
-}
-
-func (pc *poolConnectionHandler) LoadOne() (map[string]interface{}, error) {
-	return nil, nil
-}
